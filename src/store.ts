32cb817266885d023ba7ad4caf71f8ebd5dd860a
Rogelio Morrell <molekilla@gmail.com>
2022-03-15 11:40:53 -0500

    init

---
+++ src/store.ts
@@ -0,0 +1,13 @@
+import { BaseBlockstore } from 'blockstore-core/base'
+
+class DataAgentStore extends BaseBlockstore {
+  put (key, val, options) {
+    // store a block
+  }
+
+  get (key, options) {
+    // retrieve a block
+  }
+
+  // ...etc
+}
