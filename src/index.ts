5650ff91fe5e0b845d29e0908b1d059827a5eb05
Rogelio Morrell <molekilla@gmail.com>
2022-03-30 10:01:02 -0500

    es2016

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export * from './core/dagjson'
 export * from './core/db'
+export * from './core/messaging'
+export * from './interfaces/BlockCodec'
+export * from './interfaces/Blockvalue'
+export * from './interfaces/ChannelTopic'
+
